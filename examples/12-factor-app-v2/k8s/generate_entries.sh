4a87a10ea8959d113c5d482ccaf69946ddc680da
Edmund Haselwanter <ehaselwanter@infralovers.com>
2020-05-04 18:25:06 +0200

    add linebreaks

--- examples/12-factor-app-v2/k8s/generate_entries.sh
+++ examples/12-factor-app-v2/k8s/generate_entries.sh
@@ -3,20 +3,15 @@
 API_IP=$(kubectl get services api  -n 12-factor-app -o jsonpath="{.spec.clusterIP}")
 
 echo "-----------   creating a few entries  ------------"
-
-curl -XPOST "http://${API_IP}:3000/api/products" \
--H 'Content-Type: application/json' \
---data-raw '{"name": "MacBook", "price": 999}'
-curl -XPOST "http://${API_IP}:3000/api/products" \
--H 'Content-Type: application/json' \
---data-raw '{"name": "MacBook Pro", "price": 3999}'
-curl -XPOST "http://${API_IP}:3000/api/products" \
--H 'Content-Type: application/json' \
---data-raw '{"name": "Surface Pro", "price": 3999}'
-
+echo
+curl -XPOST "http://${API_IP}:3000/api/products" -H 'Content-Type: application/json' --data '{"name": "MacBook", "price": 999}'
+curl -XPOST "http://${API_IP}:3000/api/products" -H 'Content-Type: application/json' --data '{"name": "MacBook Pro", "price": 3999}'
+curl -XPOST "http://${API_IP}:3000/api/products" -H 'Content-Type: application/json' --data '{"name": "Surface Pro", "price": 3999}'
+echo
 echo "-----------   listing all the entries  ------------"
-
+echo
 curl "${API_IP}:3000/api/products"
+echo
 
 # play with jq!
 
