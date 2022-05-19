b4c588670af62fec79168cfd0bca5df992b03794
Martin Buchleitner <mabunixda@gmail.com>
2022-05-19 08:18:19 +0200

    update chaos install

--- examples/12-factor-app-v2/chaos/install.sh
+++ examples/12-factor-app-v2/chaos/install.sh
@@ -1,1 +1,5 @@
-microk8s helm3 install chaos-mesh helm/chaos-mesh --namespace=chaos-testing --set chaosDaemon.runtime=containerd --set chaosDaemon.socketPath=/var/snap/microk8s/common/run/containerd.sock
+microk8s helm3 install chaos-mesh helm/chaos-mesh --create-namespace \
+				--namespace=chaos-testing \
+				--set chaosDaemon.runtime=containerd \
+				--set chaosDaemon.socketPath=/var/snap/microk8s/common/run/containerd.sock
+
