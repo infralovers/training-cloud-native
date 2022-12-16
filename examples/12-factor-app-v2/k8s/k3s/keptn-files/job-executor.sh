54549ba5992e54e7996d44c499ddd4737aa84003
Matthias Theuermann <mtheuermann@infralovers.com>
2022-12-16 11:17:15 +0100

    fix: added job-executor config for helm

---
+++ examples/12-factor-app-v2/k8s/k3s/keptn-files/job-executor.sh
@@ -0,0 +1,8 @@
+export JES_VERSION=0.2.3
+export JES_NAMESPACE="keptn-jes"
+
+export KEPTN_API_PROTOCOL=http # or https
+export KEPTN_API_HOST=keptn.cno-user-01-01.rabbit.commandemy.training # e.g., 1.2.3.4.nip.io
+export KEPTN_API_TOKEN=$(kubectl get secret keptn-api-token -n keptn -ojsonpath={.data.keptn-api-token} | base64 --decode)
+
+export TASK_SUBSCRIPTION=sh.keptn.event.remote-task.triggered
