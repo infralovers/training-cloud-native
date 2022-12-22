2d23059f319a9736883e76948ede418adccaeb94
Matthias Theuermann <mtheuermann@infralovers.com>
2022-12-22 08:14:17 +0100

    added subscription

--- examples/12-factor-app-v2/k8s/k3s/keptn-files/job-executor.sh
+++ examples/12-factor-app-v2/k8s/k3s/keptn-files/job-executor.sh
@@ -5,4 +5,4 @@ export KEPTN_API_PROTOCOL=http # or https
 export KEPTN_API_HOST=keptn.cno-user-01-01.rabbit.commandemy.training # e.g., 1.2.3.4.nip.io
 export KEPTN_API_TOKEN=$(kubectl get secret keptn-api-token -n keptn -ojsonpath={.data.keptn-api-token} | base64 --decode)
 
-export TASK_SUBSCRIPTION=sh.keptn.event.remote-task.triggered
+export TASK_SUBSCRIPTION=sh.keptn.event.deployment.triggered,sh.keptn.event.rollback.triggered,sh.keptn.event.action.triggered,sh.keptn.event.test.triggered
