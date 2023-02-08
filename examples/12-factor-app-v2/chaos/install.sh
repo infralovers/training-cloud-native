f2a734b4cb98a032fc0f7509f4951b1c101f5dbf
Martin Buchleitner <mabunixda@gmail.com>
2023-02-08 14:23:12 +0100

    remove v1/v2

--- examples/12-factor-app-v2/chaos/install.sh
+++
@@ -1,5 +0,0 @@
-microk8s helm3 install chaos-mesh helm/chaos-mesh --create-namespace \
-				--namespace=chaos-testing \
-				--set chaosDaemon.runtime=containerd \
-				--set chaosDaemon.socketPath=/var/snap/microk8s/common/run/containerd.sock
-
