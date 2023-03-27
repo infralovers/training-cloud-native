7b1b997032eef5edccd1b662b2b19d12582e8d8d
Martin Buchleitner <mbuchleitner@infralovers.com>
2023-03-27 15:46:32 +0200

    fix: env name update (#49)
    * fix: env-name replace script update
    
    Signed-off-by: Martin Buchleitner <mbuchleitner@infralovers.com>
    
    * fix: hostname update on whoami
    
    Signed-off-by: Martin Buchleitner <mbuchleitner@infralovers.com>
    
    ---------
    
    Signed-off-by: Martin Buchleitner <mbuchleitner@infralovers.com>

--- replace-variables.sh
+++ replace-variables.sh
@@ -8,3 +8,6 @@ find examples* -type f -exec sed -i -e "s/<ENV-ANIMAL>/$ANIMAL/g" {} \;
 
 # Replace <ENV-IP>
 find examples* -type f -exec sed -i -e "s/<ENV-IP>/$HOST_IP/g" {} \;
+
+# Replace <ENV-NAME>
+find examples* -type f -exec sed -i -e "s/<ENV-NAME>/$ENVIRONMENT/g" {} \;
