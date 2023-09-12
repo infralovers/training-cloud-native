c95cd781adf8c4ed3da36fe6ddfc9ed7daea61dd
Martin Buchleitner <mabunixda@gmail.com>
2023-09-12 08:04:27 +0200

    fix: update replace script
    Signed-off-by: Martin Buchleitner <mabunixda@gmail.com>

--- replace-variables.sh
+++ replace-variables.sh
@@ -1,16 +1,18 @@
-#!/bin/sh
+#!/usr/bin/env bash
+
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
 # Replace <ENV-USER-ID>
-find examples* -type f -exec sed -i -e "s/<ENV-USER-ID>/$USER_ID/g" {} \;
+find "${SCRIPT_DIR}/examples" -type f -exec sed -i -e "s/<ENV-USER-ID>/$USER_ID/g" {} \;
 
 # Replace <ENV-ANIMAL>
-find examples* -type f -exec sed -i -e "s/<ENV-ANIMAL>/$ANIMAL/g" {} \;
+find "${SCRIPT_DIR}/examples" -type f -exec sed -i -e "s/<ENV-ANIMAL>/$ANIMAL/g" {} \;
 
 # Replace <ENV-IP>
-find examples* -type f -exec sed -i -e "s/<ENV-IP>/$HOST_IP/g" {} \;
+find "${SCRIPT_DIR}/examples" -type f -exec sed -i -e "s/<ENV-IP>/$HOST_IP/g" {} \;
 
 # Replace <ENV-NAME>
-find examples* -type f -exec sed -i -e "s/<ENV-NAME>/$ENVIRONMENT/g" {} \;
+find "${SCRIPT_DIR}/examples" -type f -exec sed -i -e "s/<ENV-NAME>/$ENVIRONMENT/g" {} \;
 
 # Replace <ENV-DOMAIN>
-find examples* -type f -exec sed -i -e "s/<ENV-DOMAIN>/$DOMAIN/g" {} \;
+find "${SCRIPT_DIR}/examples" -type f -exec sed -i -e "s/<ENV-DOMAIN>/$DOMAIN/g" {} \;
